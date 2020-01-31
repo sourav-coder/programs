869ee7751b80c0c4449ae6ac0606fe4311bc31de
SOURAV SARKAR <55509336+sourav-coder@users.noreply.github.com>
2020-01-31 12:38:04 +0530

    Create Rectangle .py

---
+++ Rectangle .py
@@ -0,0 +1,9 @@
+
+#--coded by sourav sarkar--#
+
+for _ in range(int(input())):
+    a,b,c,d=map(int,input().split())
+    if a==b and c==d:print('YES')
+    elif a==c and b==d:print('YES')
+    elif a==d and b==c:print('YES')
+    else:print('NO')
